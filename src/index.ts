718b0239f36bef024e15c94c353e29b93ebb7e92
radekBednarik <radek.bednarik@tesena.com>
2025-04-01 13:31:33 +0200

    _

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import { generate } from "./generators/images.js";
 import { readFileAsync, saveImages } from "./io/io.js";
+import { input } from "@inquirer/prompts";
 
 (async () => {
   const promptIntro = await readFileAsync("src/prompts/prompt-intro.txt");
-  const data = await generate(
-    `${promptIntro}\n
-    image of dairy products on white background`,
-  );
+  while (true) {
+    const prompt = await input({
+      message: "Enter a prompt (or 'exit' to quit):",
+    });
 
-  await saveImages(data, "png");
+    if (prompt.toLowerCase() === "exit") {
+      console.log("Exiting...");
+      break;
+    }
+
+    const data = await generate(`${promptIntro}\n${prompt}`);
+
+    await saveImages(data, "png");
+  }
 })();
