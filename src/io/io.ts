2cefce937ebd968c18f4204170e2e9f008d8fa44
radekBednarik <radek.bednarik@tesena.com>
2025-04-03 08:18:14 +0200

    _

--- src/io/io.ts
+++ src/io/io.ts
@@ -1,38 +1,38 @@
-import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
 import { mkdir, readFile, writeFile } from "fs/promises";
+import { dirname, resolve } from "path";
 import sharp from "sharp";
+import { fileURLToPath } from "url";
 import { v4 as uuidv4 } from "uuid";
 
 export const readFileAsync = async (filePath: string) => {
-	return await readFile(resolve(filePath), "utf-8");
+  return await readFile(resolve(filePath), "utf-8");
 };
 
 export const saveImages = async (
-	data: (string | undefined)[],
-	format: "png" | "jpg",
+  data: (string | undefined)[],
+  format: "png" | "jpg",
 ) => {
-	const storagePath = resolvePath("../../downloaded_images");
+  const storagePath = resolvePath("../../downloaded_images");
 
-	await mkdir(storagePath, { recursive: true });
+  await mkdir(storagePath, { recursive: true });
 
-	data.forEach(async (d, i) => {
-		if (d) {
-			const base64String = d.replace(/^data:image\/\w+;base64,/, "");
-			const buffer = Buffer.from(base64String, "base64");
-			const pngBuffer = await sharp(buffer).toFormat(`${format}`).toBuffer();
+  data.forEach(async (d, i) => {
+    if (d) {
+      const base64String = d.replace(/^data:image\/\w+;base64,/, "");
+      const buffer = Buffer.from(base64String, "base64");
+      const imgBuffer = await sharp(buffer).toFormat(`${format}`).toBuffer();
 
-			await writeFile(
-				`${storagePath}/image_${i}_${uuidv4()}.${format}`,
-				pngBuffer,
-			);
-		}
-	});
+      await writeFile(
+        `${storagePath}/image_${i}_${uuidv4()}.${format}`,
+        imgBuffer,
+      );
+    }
+  });
 };
 
 function resolvePath(path: string) {
-	const filename = fileURLToPath(import.meta.url);
-	const __dirname = dirname(filename);
+  const filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(filename);
 
-	return resolve(__dirname, path);
+  return resolve(__dirname, path);
 }
