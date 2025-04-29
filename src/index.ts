e38b94f3f8398987f0e9aa5b1d218ef71e18fcc5
radekBednarik <radek.bednarik@tesena.com>
2025-04-29 11:19:04 +0200

    _

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,52 @@
 import { generate } from "./generators/images.js";
 import { readFileAsync, saveImages } from "./io/io.js";
-import { input } from "@inquirer/prompts";
+import { input, select } from "@inquirer/prompts";
 
 (async () => {
   const promptIntro = await readFileAsync("src/prompts/prompt-intro.txt");
+
+  const howMany = await input({
+    message:
+      "How many pictures do you want to generate? Provide 1 - 10. This will apply for all subsequent prompts.",
+  });
+
+  const howManyNumber = parseInt(howMany, 10);
+  const isNumber = !isNaN(howManyNumber);
+
+  if (!isNumber) {
+    console.error("Invalid input. Please enter a valid number.");
+    return;
+  }
+
+  if (howManyNumber < 1 || howManyNumber > 10) {
+    console.error("Invalid input. Please enter a number between 1 and 10.");
+    return;
+  }
+
+  const picsFormat = (await select({
+    message:
+      "Select the image format. This will apply for all subsequent prompts.",
+    default: "png",
+    choices: [
+      {
+        name: "webp",
+        value: "webp",
+        description: "WebP format, best used in web frontend applications.",
+      },
+      {
+        name: "png",
+        value: "png",
+        description:
+          "PNG format, versatile and generally recommended format for online pictures.",
+      },
+      {
+        name: "jpeg",
+        value: "jpeg",
+        description:
+          "JPEG format, best used for photographs and images with gradients.",
+      },
+    ],
+  })) as "webp" | "png" | "jpeg";
 
   while (true) {
     try {
@@ -16,12 +59,20 @@ import { input } from "@inquirer/prompts";
         break;
       }
 
-      const data = await generate(`${promptIntro}\n${prompt}`);
+      const data = await generate(
+        `${promptIntro}\n${prompt}`,
+        howManyNumber,
+        picsFormat,
+      );
 
       if (data) {
-        const filepaths = await saveImages(data);
+        const filepaths = await saveImages(data, picsFormat);
 
-        console.log("Images saved successfully to:\n", filepaths.join("\n"));
+        console.log(
+          "Images saved successfully to:\n",
+          filepaths.join("\n"),
+          "\n",
+        );
       } else {
         console.error("Failed to generate images.");
       }
