c6f0cfb7a379e4ee0be5f986bcd540700740ba52
radekBednarik <radek.bednarik@tesena.com>
2025-04-29 10:34:44 +0200

    _

--- src/generators/images.ts
+++ src/generators/images.ts
@@ -6,15 +6,19 @@ const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
 
-export const generate = async (prompt: string, howMany = 1) => {
+export const generate = async (
+  prompt: string,
+  howMany = 1,
+  output_format: "png" | "webp" | "jpeg" = "png",
+) => {
   const spinner = ora("Generating images...").start();
 
   const response = await openai.images.generate({
     model: "gpt-image-1",
     prompt,
+    output_format,
     n: howMany,
     size: "1024x1024",
-    output_format: "png",
     quality: "high",
     moderation: "low",
   });
